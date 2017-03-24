44017c3c6399fa19eec4742e846fed22e3457048
Coyote05 <coyote0505@gmail.com>
2017-03-25 00:08:59 +0100

    #7

---
+++ src/homework02/CalculatorService.java
@@ -0,0 +1,20 @@
+package homework02;
+
+
+public class CalculatorService {
+
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        if (number % 2 == 0){
+
+            System.out.println("true");
+        }
+
+        else{
+
+            System.out.println("false");
+        }
+    }
+}
