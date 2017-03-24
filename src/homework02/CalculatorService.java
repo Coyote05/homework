8f7aa4ba866d3204af41bb6f880c817d64d33d52
Coyote05 <coyote0505@gmail.com>
2017-03-25 00:26:49 +0100

    #7

--- src/homework02/CalculatorService.java
+++ src/homework02/CalculatorService.java
@@ -5,6 +5,10 @@ public class CalculatorService {
 
     public static void main(String[] args) {
 
+        /*
+        Egy adoot számról eldönti páros-e. Futtatáskor igaz vagy hamis érték kerül kiírása.
+         */
+
         int number = 5;
 
         if (number % 2 == 0){
@@ -18,3 +22,4 @@ public class CalculatorService {
         }
     }
 }
+
